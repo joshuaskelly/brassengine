7d4e0f593224e91e5dd09e260c1d68d293a884e0
Joshua Skelton <joshua.skelton@gmail.com>
2022-09-15 21:06:24 -0700

    Support for creating new textures from Lua

--- src/bindings/texture.c
+++ src/bindings/texture.c
@@ -6,6 +6,45 @@
 
 #include "../graphics.h"
 
+/**
+ * Texture garbage collection metamethod.
+ *
+ * @param L Lua VM
+ * @return int
+ */
+int texture_gc(lua_State* L) {
+    texture_t** texture = lua_touserdata(L, 1);
+    graphics_texture_free(*texture);
+
+    return 0;
+}
+
+/**
+ * Create new texture.
+ *
+ * @param width Texture width
+ * @param height Texture height
+ * @return Texture userdata
+ */
+int bindings_texture_new(lua_State* L) {
+    int width = (int)lua_tonumber(L, 1);
+    int height = (int)lua_tonumber(L, 2);
+
+    lua_pop(L, -1);
+
+    texture_t** tp = (texture_t**)lua_newuserdata(L, sizeof(texture_t*));
+    *tp = graphics_texture_new(width, height, NULL);
+    luaL_setmetatable(L, "texture_userdata_metatable");
+
+    return 1;
+}
+
+/**
+ * Fill entire texture with color.
+ *
+ * @param texture
+ * @param color
+ */
 int bindings_texture_clear(lua_State* L) {
     texture_t** texture = lua_touserdata(L, 1);
     int color = (int)lua_tonumber(L, 2);
@@ -72,6 +111,7 @@ int bindings_texture_blit(lua_State* L) {
 }
 
 static const struct luaL_Reg module_functions[] = {
+    {"new", bindings_texture_new},
     {"clear", bindings_texture_clear},
     {"set_pixel", bindings_texture_set_pixel},
     {"get_pixel", bindings_texture_get_pixel},
@@ -81,5 +121,14 @@ static const struct luaL_Reg module_functions[] = {
 
 int open_texture_module(lua_State* L) {
     luaL_newlib(L, module_functions);
+
+    // Push texture userdata metatable
+    luaL_newmetatable(L, "texture_userdata_metatable");
+    lua_pushstring(L, "__gc");
+    lua_pushcfunction(L, texture_gc);
+    lua_settable(L, -3);
+
+    lua_pop(L, 1);
+
     return 1;
 }
