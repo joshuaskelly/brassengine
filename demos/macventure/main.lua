14478c6bcb81ba52cf9ff950232731040c3dd62d
Joshua Skelton <joshua.skelton@gmail.com>
2022-10-08 20:59:06 -0700

    Added basic cursor

--- demos/macventure/main.lua
+++ demos/macventure/main.lua
@@ -1,5 +1,7 @@
+local assets = require("assets")
 local draw = require("draw")
 local graphics = require("graphics")
+local mouse = require("input.mouse")
 local chevyfont = require("chevyfont")
 
 -- Called once at startup
@@ -9,6 +11,9 @@ function _init()
     font = chevyfont.new("fonts/skullboy_mono/atlas8.gif", "fonts/skullboy_mono/metrics.json")
     font.size = 12
     --font = chevyfont.new("fonts/thicket_mono/atlas8.gif", "fonts/thicket_mono/metrics.json")
+
+    cursor = assets.get_texture("cursor.gif")
+    graphics.set_transparent_color(0)
 end
 
 -- Called once per frame
@@ -22,4 +27,7 @@ function _draw()
     font:draw("The last thing that you", rt, 0, 0 * font.size)
     font:draw("remember is standing", rt, 0, 1 * font.size)
     font:draw("before the wizard Lakmir", rt, 0, 2 * font.size)
+
+    local x, y = mouse.position()
+    graphics.blit(cursor, 0, 0, 16, 16, x - 4, y, 16, 16)
 end
