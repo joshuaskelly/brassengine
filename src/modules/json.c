70ea18e7c3996dfd5c778c8e2f7273958f28183d
Joshua Skelton <joshua.skelton@gmail.com>
2022-12-05 11:02:33 -0800

    Remove unused ifdef

--- src/modules/json.c
+++ src/modules/json.c
@@ -2014,12 +2014,6 @@ static int luaopen_cjson(lua_State *l)
 {
     lua_cjson_new(l);
 
-#ifdef ENABLE_CJSON_GLOBAL
-    /* Register a global "cjson" table. */
-    lua_pushvalue(l, -1);
-    lua_setglobal(l, CJSON_MODNAME);
-#endif
-
     /* Return cjson table */
     return 1;
 }
