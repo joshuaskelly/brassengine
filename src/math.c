92ece2de12760376036aacc4d53be6137cebea47
Joshua Skelton <joshua.skelton@gmail.com>
2023-01-06 12:08:27 -0800

    Added math utils module

---
+++ src/math.c
@@ -0,0 +1,5 @@
+#include "math.h"
+
+float frac(float f) {
+    return f - (int)f;
+}
---
+++ src/math.h
@@ -0,0 +1,12 @@
+#ifndef MATH_H
+#define MATH_H
+
+/**
+ * Returns fractional part of float.
+ *
+ * @param f Float value
+ * @return float Fraction part of value between 0 and 1
+ */
+float frac(float f);
+
+#endif
