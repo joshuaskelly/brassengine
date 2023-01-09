01b1411ef151a9e912bf8eff900260337d757934
Joshua Skelton <joshua.skelton@gmail.com>
2023-01-09 13:29:39 -0800

    :lipstick:

--- src/event.c
+++ src/event.c
@@ -6,7 +6,7 @@ static event_t queue[MAXEVENTS];
 static int head = 0;
 static int tail = 0;
 
-void event_post(event_t*  event) {
+void event_post(event_t* event) {
     queue[head] = *event;
     head = (head + 1) % MAXEVENTS;
 }
@@ -15,7 +15,7 @@ bool event_poll(event_t* event) {
     if (tail == head) return false;
 
     *event = queue[tail];
-    tail = (tail +  1) % MAXEVENTS;
+    tail = (tail + 1) % MAXEVENTS;
 
     return true;
 }
