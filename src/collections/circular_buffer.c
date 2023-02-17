6dad46f911c7953e7a783d51e0e056c21878cccc
Joshua Skelton <joshua.skelton@gmail.com>
2023-02-17 14:31:51 -0800

    Adding api documentation

--- src/collections/circular_buffer.c
+++ src/collections/circular_buffer.c
@@ -4,10 +4,10 @@
 
 #include "circular_buffer.h"
 
-circular_buffer_t* circular_buffer_new(size_t count) {
-    circular_buffer_t* buffer = (circular_buffer_t*)malloc(sizeof(int) * 2 + sizeof(size_t) * 2 + sizeof(void*) * count);
-    buffer->max_count = count;
-    memset(buffer->data, 0, sizeof(void*) * count);
+circular_buffer_t* circular_buffer_new(size_t capacity) {
+    circular_buffer_t* buffer = (circular_buffer_t*)malloc(sizeof(int) * 2 + sizeof(size_t) * 2 + sizeof(void*) * capacity);
+    buffer->capacity = capacity;
+    memset(buffer->data, 0, sizeof(void*) * capacity);
     circular_buffer_clear(buffer);
 
     return buffer;
@@ -24,7 +24,7 @@ void circular_buffer_clear(circular_buffer_t* buffer) {
     buffer->tail = 0;
     buffer->count = 0;
 
-    for (int i = 0; i < buffer->max_count; i++) {
+    for (int i = 0; i < buffer->capacity; i++) {
         if (buffer->data[i]) {
             free(buffer->data[i]);
         }
@@ -35,21 +35,21 @@ void circular_buffer_clear(circular_buffer_t* buffer) {
 void circular_buffer_add(circular_buffer_t* buffer, void* item) {
     buffer->data[buffer->tail] = item;
 
-    if (buffer->count < buffer->max_count - 1) {
+    if (buffer->count < buffer->capacity - 1) {
         buffer->count++;
     }
 
-    int next = (buffer->tail + 1) % buffer->max_count;
+    int next = (buffer->tail + 1) % buffer->capacity;
     if (next == buffer->head) {
         free(buffer->data[buffer->head]);
         buffer->data[buffer->head] = NULL;
-        buffer->head = (buffer->head + 1) % buffer->max_count;
+        buffer->head = (buffer->head + 1) % buffer->capacity;
     }
 
     buffer->tail = next;
 }
 
 void* circular_buffer_get(circular_buffer_t* buffer, int index) {
-    int i = (buffer->head + index) % buffer->max_count;
+    int i = (buffer->head + index) % buffer->capacity;
     return buffer->data[i];
 }
--- src/collections/circular_buffer.h
+++ src/collections/circular_buffer.h
@@ -5,21 +5,59 @@
 #include <stdlib.h>
 
 typedef struct {
+    /** Index of first element. */
     int head;
+
+    /** Index of last element.*/
     int tail;
+
+    /** Number of items in buffer. */
     size_t count;
-    size_t max_count;
+
+    /** Maximum number of items buffer can contain. */
+    size_t capacity;
+
+    /** Buffer items. */
     void* data[];
 } circular_buffer_t;
 
-circular_buffer_t* circular_buffer_new(size_t count);
+/**
+ * Creates a circular buffer.
+ *
+ * @param capacity Maximum number of elements
+ * @return circular_buffer_t* New circular buffer
+ */
+circular_buffer_t* circular_buffer_new(size_t capacity);
 
+/**
+ * Frees a circular buffer.
+ *
+ * @param buffer Circular buffer to free.
+ */
 void circular_buffer_free(circular_buffer_t* buffer);
 
+/**
+ * Removes all items from circular buffer.
+ *
+ * @param buffer Circular buffer to clear.
+ */
 void circular_buffer_clear(circular_buffer_t* buffer);
 
+/**
+ * Adds an item to a circular buffer.
+ *
+ * @param buffer Circular buffer to add to.
+ * @param item Item to add to circular buffer.
+ */
 void circular_buffer_add(circular_buffer_t* buffer, void* item);
 
+/**
+ * Gets an item from a circular buffer.
+ *
+ * @param buffer Circular buffer to get item from.
+ * @param index Index of item to get.
+ * @return void* Item from circular buffer.
+ */
 void* circular_buffer_get(circular_buffer_t* buffer, int index);
 
 #endif
