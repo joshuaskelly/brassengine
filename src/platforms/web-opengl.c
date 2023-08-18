3d3193e82ab18c344c1468a175931e3da625ec33
Joshua Skelton <joshua.skelton@gmail.com>
2023-08-17 21:26:26 -0700

    Fix webgl black texture issue

--- src/platforms/web-opengl.c
+++ src/platforms/web-opengl.c
@@ -143,6 +143,8 @@ void platform_init(void) {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, config->resolution.width, config->resolution.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, render_buffer);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
     display_rect.x = 0;
     display_rect.y = 0;
