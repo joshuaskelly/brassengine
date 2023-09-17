46363fba10fd1008bcc5fe9cda072259cb38747c
Joshua Skelton <joshua.skelton@gmail.com>
2023-09-16 22:20:55 -0700

    Remove old API

--- src/modules/raycaster.c
+++ src/modules/raycaster.c
@@ -17,168 +17,12 @@
 
 #include "../renderers/raycaster.h"
 
-raycaster_camera_t* luaL_checkraycastercamera(lua_State* L, int index) {
-    raycaster_camera_t* camera = NULL;
-    luaL_checktype(L, index, LUA_TUSERDATA);
-    camera = (raycaster_camera_t*)luaL_checkudata(L, index, "raycaster_camera");
-
-    if (!camera) {
-        luaL_typeerror(L, index, "raycaster_camera");
-    }
-
-    return camera;
-}
-
-static int modules_raycaster_camera_meta_index(lua_State* L) {
-    raycaster_camera_t* camera = luaL_checkraycastercamera(L, 1);
-    const char* key = luaL_checkstring(L, 2);
-
-    lua_pop(L, -1);
-
-    if (strcmp(key, "position") == 0) {
-        lua_newvector2(L, camera->position[0], camera->position[1]);
-    }
-    else if (strcmp(key, "direction") == 0) {
-        lua_newvector2(L, camera->direction[0], camera->direction[1]);
-    }
-    else if (strcmp(key, "fov") == 0) {
-        lua_pushnumber(L, camera->fov);
-    }
-    else {
-        lua_pushnil(L);
-    }
-
-    return 1;
-}
-
-static int modules_raycaster_camera_meta_newindex(lua_State* L) {
-    raycaster_camera_t* camera = luaL_checkraycastercamera(L, 1);
-    const char* key = luaL_checkstring(L, 2);
-
-    if (strcmp(key, "position") == 0) {
-        mfloat_t* vector = luaL_checkvector2(L, 3);
-        vec2_assign(camera->position, vector);
-    }
-    else if (strcmp(key, "direction") == 0) {
-        mfloat_t* vector = luaL_checkvector2(L, 3);
-        vec2_assign(camera->direction, vector);
-    }
-    else if (strcmp(key, "fov") == 0) {
-        float number = luaL_checknumber(L, 3);
-        camera->fov = number;
-    }
-    else {
-        luaL_error(L, "attempt to index a raycaster_camera value");
-    }
-
-    lua_pop(L, -1);
-
-    return 0;
-}
-
-static const struct luaL_Reg raycaster_camera_meta_functions[] = {
-    {"__index", modules_raycaster_camera_meta_index},
-    {"__newindex", modules_raycaster_camera_meta_newindex},
-    {NULL, NULL}
-};
-
-raycaster_sprite_t* luaL_checkraycastersprite(lua_State* L, int index) {
-    raycaster_sprite_t* sprite = NULL;
-    luaL_checktype(L, index, LUA_TUSERDATA);
-    sprite = (raycaster_sprite_t*)luaL_checkudata(L, index, "raycaster_sprite");
-
-    if (!sprite) {
-        luaL_typeerror(L, index, "raycaster_sprite");
-    }
-
-    return sprite;
-}
-
-static int lua_newraycastersprite(lua_State* L) {
-    raycaster_sprite_t* sprite = (raycaster_sprite_t*)lua_newuserdata(L, sizeof(raycaster_sprite_t));
-    sprite->texture = NULL;
-    sprite->position[0] = 0;
-    sprite->position[1] = 0;
-    sprite->distance = FLT_MAX;
-    luaL_setmetatable(L, "raycaster_sprite");
-
-    return 1;
-}
-
-static int modules_raycaster_sprite_meta_index(lua_State* L) {
-    raycaster_sprite_t* sprite = luaL_checkraycastersprite(L, 1);
-    const char* key = luaL_checkstring(L, 2);
-
-    lua_pop(L, -1);
-
-    if (strcmp(key, "position") == 0) {
-        lua_newvector2(L, sprite->position[0], sprite->position[1]);
-    }
-    else if (strcmp(key, "texture") == 0) {
-        lua_pushtexture(L, sprite->texture);
-    }
-    else {
-        lua_pushnil(L);
-    }
-
-    return 1;
-}
-
-static int modules_raycaster_sprite_meta_newindex(lua_State* L) {
-    raycaster_sprite_t* sprite = luaL_checkraycastersprite(L, 1);
-    const char* key = luaL_checkstring(L, 2);
-
-    if (strcmp(key, "position") == 0) {
-        mfloat_t* vector = luaL_checkvector2(L, 3);
-        vec2_assign(sprite->position, vector);
-    }
-    else if (strcmp(key, "texture") == 0) {
-        texture_t* texture = luaL_checktexture(L, 3);
-        sprite->texture = texture;
-    }
-    else {
-        luaL_error(L, "attempt to index a raycaster_sprite value");
-    }
-
-    lua_pop(L, -1);
-
-    return 0;
-}
-
-static const struct luaL_Reg raycaster_sprite_meta_functions[] = {
-    {"__index", modules_raycaster_sprite_meta_index},
-    {"__newindex", modules_raycaster_sprite_meta_newindex},
-    {NULL, NULL}
-};
-
 raycaster_map_t* luaL_checkraycastermap(lua_State* L, int index) {
     raycaster_map_t** handle = NULL;
     luaL_checktype(L, index, LUA_TUSERDATA);
     handle = (raycaster_map_t**)luaL_checkudata(L, index, "raycaster_map");
 
     return *handle;
-}
-
-static int modules_raycaster_map_add_sprite(lua_State* L) {
-    raycaster_map_t* map = luaL_checkraycastermap(L, 1);
-    raycaster_sprite_t* sprite = luaL_checkraycastersprite(L, 2);
-
-    lua_pop(L, -1);
-
-    raycaster_map_add_sprite(map, sprite);
-
-    return 0;
-}
-
-static int modules_raycaster_map_remove_sprite(lua_State* L) {
-    raycaster_map_t* map = luaL_checkraycastermap(L, 1);
-    raycaster_sprite_t* sprite = luaL_checkraycastersprite(L, 2);
-
-    lua_pop(L, -1);
-
-    raycaster_map_remove_sprite(map, sprite);
-
-    return 0;
 }
 
 static int modules_raycaster_map_meta_index(lua_State* L) {
@@ -189,12 +33,6 @@ static int modules_raycaster_map_meta_index(lua_State* L) {
 
     if (strcmp(key, "walls") == 0) {
         lua_pushtexture(L, map->walls);
-    }
-    else if (strcmp(key, "add_sprite") == 0) {
-        lua_pushcfunction(L, modules_raycaster_map_add_sprite);
-    }
-    else if (strcmp(key, "remove_sprite") == 0) {
-        lua_pushcfunction(L, modules_raycaster_map_remove_sprite);
     }
     else {
         lua_pushnil(L);
@@ -249,121 +87,6 @@ static int modules_raycaster_map_new(lua_State* L) {
 }
 
 /**
- * Create a new raycaster_camera object.
- * @function camera_new
- * @return raycaster_camera
- */
-static int modules_raycaster_camera_new(lua_State* L) {
-    raycaster_camera_t* camera = (raycaster_camera_t*)lua_newuserdata(L, sizeof(raycaster_camera_t));
-    camera->position[0] = 0;
-    camera->position[1] = 0;
-    camera->direction[0] = 1;
-    camera->direction[1] = 0;
-    camera->fov = 90;
-    luaL_setmetatable(L, "raycaster_camera");
-
-    return 1;
-}
-
-/**
- * Create a new raycaster_sprite object.
- * @function sprite_new
- * @return raycaster_sprite
- */
-static int modules_raycaster_sprite_new(lua_State* L) {
-    lua_newraycastersprite(L);
-
-    return 1;
-}
-
-/**
- * Renders given map data.
- * @function render
- * @param camera Camera to render with
- * @param map Map as texture
- * @param texture Render texture
- */
-
-/**
- * Renders given map data.
- * @function render
- * @param camera Camera to render with
- * @param map Map as texture
- * @param texture Render texture
- * @param rx Render texture x-offset
- * @param ry Render texture y-offset
- * @param rw Render texture width
- * @param rh Render texture height
- */
-static int module_raycaster_render(lua_State* L) {
-    raycaster_camera_t* camera = luaL_checkraycastercamera(L, 1);
-    raycaster_map_t* map = luaL_checkraycastermap(L, 2);
-    texture_t* render_texture = luaL_checktexture(L, 3);
-    float rx = luaL_optnumber(L, 4, 0);
-    float ry = luaL_optnumber(L, 5, 0);
-    float rw = luaL_optnumber(L, 6, render_texture->width);
-    float rh = luaL_optnumber(L, 7, render_texture->height);
-
-    lua_pop(L, 1);
-
-    rect_t rect = {rx, ry, rw, rh};
-
-    raycaster_render(
-        camera,
-        map,
-        render_texture,
-        &rect
-    );
-
-    return 0;
-}
-
-/**
- * Sets texture in texture palette.
- * @function palette
- * @param index Texture palette index
- * @param texture Texture for given index
- */
-static int module_raycaster_palette(lua_State* L) {
-    int i = (int)luaL_checknumber(L, 1);
-    texture_t* texture = luaL_checktexture(L, 2);
-
-    lua_pop(L, 1);
-
-    raycaster_set_texture(i, texture);
-
-    return 0;
-}
-
-/**
- * Sets texture to use as a color lookup table for shading.
- * @function shade_table
- * @param texture Texture to use as lookup table.
- */
-static int module_raycaster_shade_table_set(lua_State* L) {
-    texture_t* texture = luaL_checktexture(L, 1);
-
-    lua_pop(L, 1);
-
-    raycaster_shade_table_set(texture);
-
-    return 0;
-}
-
-/**
- * Sets far distance for fog.
- * @function fog_distance
- * @param distance Distance where fog effect is greatest.
- */
-static int module_raycaster_fog_distance_set(lua_State* L) {
-    float distance = luaL_checknumber(L, 1);
-
-    raycaster_fog_distance_set(distance);
-
-    return 0;
-}
-
-/**
  * Map for rendering a raycaster scene.
  * @type raycaster_map
  */
@@ -373,61 +96,8 @@ static int module_raycaster_fog_distance_set(lua_State* L) {
  * @field walls A texture userdata representing wall positions and textures.
  */
 
-/**
- * Add given sprite to map.
- * @function add_sprite
- * @param sprite A raycaster_sprite
- */
-
-/**
- * Remove given sprite from map.
- * @function remove_sprite
- * @param sprite A raycaster_sprite
- */
-
-/**
- * Camera for rendering a raycaster scene.
- * @type raycaster_camera
- */
-
-/**
- * Camera position.
- * @field position A vector2 userdata representing camera position.
- */
-
-/**
- * Camera direction.
- * @field direction A vector2 userdata representing camera direction.
- */
-
-/**
- * Camera field of view.
- * @field fov Camera field of view in degrees.
- */
-
-/**
- * Sprite for rendering in a raycaster scene.
- * @type raycaster_sprite
- */
-
-/**
- * Sprite position.
- * @field position A vector2 userdata representing sprite position.
- */
-
-/**
- * Sprite texture.
- * @field texture
- */
-
 static const struct luaL_Reg module_functions[] = {
-    {"render", module_raycaster_render},
-    {"palette", module_raycaster_palette},
-    {"shade_table", module_raycaster_shade_table_set},
-    {"fog_distance", module_raycaster_fog_distance_set},
     {"map_new", modules_raycaster_map_new},
-    {"camera_new", modules_raycaster_camera_new},
-    {"sprite_new", modules_raycaster_sprite_new},
     {NULL, NULL}
 };
 
@@ -436,14 +106,6 @@ int luaopen_raycaster(lua_State* L) {
 
     luaL_newmetatable(L, "raycaster_map");
     luaL_setfuncs(L, raycaster_map_meta_functions, 0);
-    lua_pop(L, 1);
-
-    luaL_newmetatable(L, "raycaster_camera");
-    luaL_setfuncs(L, raycaster_camera_meta_functions, 0);
-    lua_pop(L, 1);
-
-    luaL_newmetatable(L, "raycaster_sprite");
-    luaL_setfuncs(L, raycaster_sprite_meta_functions, 0);
     lua_pop(L, 1);
 
     return 1;
--- src/renderers/raycaster.h
+++ src/renderers/raycaster.h
@@ -11,20 +11,7 @@
 #include "../collections/list.h"
 
 typedef struct {
-    mfloat_t position[VEC2_SIZE];
-    mfloat_t direction[VEC2_SIZE];
-    float fov;
-} raycaster_camera_t;
-
-typedef struct {
-    texture_t* texture;
-    mfloat_t position[VEC2_SIZE];
-    float distance;
-} raycaster_sprite_t;
-
-typedef struct {
     texture_t* walls;
-    list_t* sprites;
 } raycaster_map_t;
 
 /**
@@ -40,71 +27,6 @@ raycaster_map_t* raycaster_map_new(void);
  * @param map Map to free.
  */
 void raycaster_map_free(raycaster_map_t* map);
-
-/**
- * Adds a sprite to a map.
- *
- * @param map Map to add sprite to.
- * @param sprite Sprite to add.
- */
-void raycaster_map_add_sprite(raycaster_map_t* map, raycaster_sprite_t* sprite);
-
-/**
- * Removes a sprite from a map.
- *
- * @param map Map to remove sprite from.
- * @param sprite Sprite to remove.
- */
-void raycaster_map_remove_sprite(raycaster_map_t* map, raycaster_sprite_t* sprite);
-
-/**
- * Renders given map
- *
- * @param camera Camera to render from
- * @param map Map to render
- * @param render_texture Texture to render to. NULL will use default render target
- * @param render_rect Portion of render texture to render to. NULL will use entire render target
- */
-void raycaster_render(
-    raycaster_camera_t* camera,
-    raycaster_map_t* map,
-    texture_t* render_texture,
-    rect_t* render_rect
-);
-
-/**
- * Returns a texture in the texture palette for given index.
- *
- * @param i Index of texture.
- * @return texture_t* Texture from palette.
- */
-texture_t* raycaster_get_texture(int i);
-
-/**
- * Sets a texture palette at given index to given texture.
- *
- * @param i Index in palette to set
- * @param texture Texture to set
- */
-void raycaster_set_texture(int i, texture_t* texture);
-
-/**
- * Gets shade table.
- *
- * @return texture_t* Shade table as a texture.
- */
-texture_t* raycaster_shade_table_get(void);
-
-/**
- * Sets shade table.
- *
- * @param texture Texture to use as the shade table.
- */
-void raycaster_shade_table_set(texture_t* texture);
-
-float raycaster_fog_distance_get(void);
-
-void raycaster_fog_distance_set(float distance);
 
 typedef struct {
     texture_t* render_texture;
