abf4c430c2862305080a78ef7f830ea9fe79ad75
Joshua Skelton <joshua.skelton@gmail.com>
2023-10-23 22:48:54 -0400

    Comment pass

--- src/sounds.h
+++ src/sounds.h
@@ -1,3 +1,8 @@
+/**
+ * @file sounds.h
+ * Sounds module. Responsible for working with sound.
+ */
+
 #ifndef SOUNDS_H
 #define SOUNDS_H
 
@@ -12,7 +17,21 @@ typedef struct {
     sample_t pcm[];
 } sound_t;
 
+/**
+ * Create a new sound.
+ *
+ * @param frame_count Number of frames.
+ * @param channel_count Number of channels.
+ * @param pcm PCM data to copy or NULL.
+ * @return New sound if successful, NULL otherwise.
+ */
 sound_t* sounds_sound_new(uint64_t frame_count, uint16_t channel_count, sample_t* pcm);
+
+/**
+ * Frees a sound.
+ *
+ * @param sound Sound to free
+ */
 void sounds_sound_free(sound_t* sound);
 
 #endif
