39f43f35b90b0a88e30552c11f28adb86907c636
Joshua Skelton <joshua.skelton@gmail.com>
2023-11-01 13:12:28 -0700

    Remove temp sound assets

--- assets/main.lua
+++ assets/main.lua
@@ -1,25 +1,11 @@
-local assets = require("assets")
 local draw = require("draw")
-local keyboard = require("keyboard")
-local sound = require("sound")
 
 -- Called once at startup
 function _init()
-    s = assets.get_sound("sound.wav")
 end
-
-local key_down = false
 
 -- Called once per frame
 function _update()
-    if keyboard.key(44) then
-        if not key_down then
-            sound.play(s)
-            key_down = true
-        end
-    else
-        key_down = false
-    end
 end
 
 -- Called once per frame
