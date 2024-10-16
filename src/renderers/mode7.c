f87f04fd77b58626270c39abb87d78fed406c0c4
Joshua Skelton <joshua.skelton@gmail.com>
2024-10-15 20:30:35 -0700

    Cleanup draw_scanline

--- src/renderers/mode7.c
+++ src/renderers/mode7.c
@@ -26,7 +26,7 @@ void mode7_renderer_free(mode7_renderer_t* renderer) {
     renderer = NULL;
 }
 
-void draw_row(mode7_renderer_t* renderer, int y, float u0, float v0, float u1, float v1, texture_t* texture);
+void draw_scanline(mode7_renderer_t* renderer, int y, float u0, float v0, float u1, float v1, texture_t* texture);
 
 void mode7_renderer_render(mode7_renderer_t* renderer, texture_t* texture, mode7_callback_t callback) {
     mfloat_t inverse[MAT3_SIZE];
@@ -52,7 +52,7 @@ void mode7_renderer_render(mode7_renderer_t* renderer, texture_t* texture, mode7
         vec3(work, renderer->render_texture->width - 1, y, 1);
         vec3_multiply_mat3(uv1, work, inverse);
 
-        draw_row(
+        draw_scanline(
             renderer,
             y,
             uv0[0], uv0[1],
@@ -62,33 +62,25 @@ void mode7_renderer_render(mode7_renderer_t* renderer, texture_t* texture, mode7
     }
 }
 
-void draw_row(mode7_renderer_t* renderer, int y, float u0, float v0, float u1, float v1, texture_t* texture) {
+void draw_scanline(mode7_renderer_t* renderer, int y, float u0, float v0, float u1, float v1, texture_t* texture) {
     // DDA based line drawing algorithm
     texture_t* render_texture = renderer->render_texture;
 
-    int x0 = 0;
-    int x1 = render_texture->width - 1;
+    int width = render_texture->width - 1;
 
-    int delta_x = x1 - x0;
-    int delta_y = 0;
-    int longest_side = fmax(abs(delta_x), abs(delta_y));
-
-    float x_inc = delta_x / (float)longest_side;
-    float y_inc = delta_y / (float)longest_side;
-
-    float current_x = x0 + 0.5f;
+    float current_x = 0.5f;
     float current_y = y + 0.5f;
 
-    float s0 = u0 * texture->width;
-    float t0 = v0 * texture->height;
-    float s1 = u1 * texture->width;
-    float t1 = v1 * texture->height;
+    float s0 = u0 * (texture->width - 1);
+    float t0 = v0 * (texture->height - 1);
+    float s1 = u1 * (texture->width - 1);
+    float t1 = v1 * (texture->height - 1);
 
     float delta_s = s1 - s0;
     float delta_t = t1 - t0;
     float st_longest_side = fmax(fabs(delta_s), fabs(delta_t));
 
-    float r = st_longest_side / longest_side;
+    float r = st_longest_side / width;
 
     float s_inc = delta_s / st_longest_side * r;
     float t_inc = delta_t / st_longest_side * r;
@@ -96,13 +88,12 @@ void draw_row(mode7_renderer_t* renderer, int y, float u0, float v0, float u1, f
     float current_s = s0 + 0.5f;
     float current_t = t0 + 0.5f;
 
-    for (int i = 0; i <= longest_side; i++) {
+    for (int i = 0; i <= width; i++) {
         color_t c = graphics_texture_pixel_get(texture, current_s, current_t);
 
         graphics_texture_pixel_set(renderer->render_texture, current_x, current_y, c);
 
-        current_x += x_inc;
-        current_y += y_inc;
+        current_x++;
         current_s += s_inc;
         current_t += t_inc;
     }
