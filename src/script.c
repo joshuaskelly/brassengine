b4818e177e2642863c007493243ba1addb33f460
Joshua Skelton <joshua.skelton@gmail.com>
2024-10-29 14:17:50 -0700

    Initial implementation of include metatable chain for autocomplete

--- src/script.c
+++ src/script.c
@@ -407,7 +407,7 @@ void script_complete(char* expression) {
     if (last_dot == NULL) {
         dot_position = 0;
         lua_getglobal(L, LUA_GNAME);
-        if (lua_type(L, 1) != LUA_TTABLE) goto done;
+        if (!lua_istable(L, 1)) goto done;
     }
     // Evaluate the root. If the result is a table, use it.
     else {
@@ -428,25 +428,41 @@ void script_complete(char* expression) {
         if (status != LUA_OK) goto done;
 
         // Ensure result is what we expect
-        if (lua_type(L, 1) != LUA_TTABLE) goto done;
+        if (!(lua_type(L, 1) == LUA_TTABLE || lua_type(L, 1) == LUA_TUSERDATA)) goto done;
+    }
+
+    // If result is userdata, check if it has a metatable
+    if (lua_isuserdata(L, 1)) {
+        if (!lua_getmetatable(L, 1)) goto done;
+        lua_remove(L, 1);
     }
 
     const char* suggestions[MAX_SUGGESTIONS];
 
     int min_suggestion_length = 2048;
 
-    // Iterate table key/value pairs
     int base = lua_gettop(L);
-    lua_pushnil(L);
     size_t size = strlen(partial);
-    while (lua_next(L, base) != 0) {
-        const char* key = luaL_checkstring(L, -2);
-        lua_pop(L, 1);
 
-        if (strncmp(partial, key, size) == 0) {
-            suggestions[suggestion_count++] = key;
-            min_suggestion_length = fminf(strlen(key), min_suggestion_length);
+    // Traverse table + metatable
+    while (lua_istable(L, 1)) {
+        // Iterate table key/value pairs
+        lua_pushnil(L);
+        while (lua_next(L, base) != 0) {
+            const char* key = luaL_checkstring(L, -2);
+            lua_pop(L, 1);
+
+            if (strncmp(partial, key, size) == 0) {
+                suggestions[suggestion_count++] = key;
+                min_suggestion_length = fminf(strlen(key), min_suggestion_length);
+            }
         }
+
+        // If we have a metatable, iterate that next
+        if (!lua_getmetatable(L, 1)) break;
+
+        // Remove previous table
+        lua_remove(L, 1);
     }
 
     if (suggestion_count == 0) goto done;
@@ -463,6 +479,9 @@ void script_complete(char* expression) {
         // Display suggestions
         char* sep = dot_position ? "." : "";
         for (int i = 0; i < suggestion_count; i++) {
+            // Skip if suggestion is same as previous
+            if (i > 0 && strcmp(suggestions[i], suggestions[i - 1]) == 0) continue;
+
             log_info("%s%s%s", root, sep, suggestions[i]);
         }
 
