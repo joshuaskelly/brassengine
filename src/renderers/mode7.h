44effa56f0b107e248960cbd0f4f197874441cc0
Joshua Skelton <joshua.skelton@gmail.com>
2024-12-28 14:49:38 -0800

    Doc pass

--- src/renderers/mode7.h
+++ src/renderers/mode7.h
@@ -24,12 +24,36 @@ typedef struct {
 
 typedef void(*mode7_callback_t)(int);
 
+/**
+ * Creates a new renderer.
+ *
+ * @return mode7_renderer_t Newly created renderer.
+ */
 mode7_renderer_t* mode7_renderer_new(texture_t* render_texture);
 
-void mode7_renderer_clear_color(mode7_renderer_t* renderer, color_t color);
-
+/**
+ * Frees a renderer.
+ *
+ * @param renderer Renderer to free.
+ */
 void mode7_renderer_free(mode7_renderer_t* renderer);
 
+/**
+ * Clears color buffer with given color.
+ *
+ * @param renderer Renderer to clear color buffer.
+ * @param color Clear color.
+ */
+void mode7_renderer_clear_color(mode7_renderer_t* renderer, color_t color);
+
+/**
+ * Render given texture and horizontal scanline callback. Callback will be
+ * invoked at the start of each horizontal scanline in the render texture.
+ *
+ * @param renderer Renderer to render to.
+ * @param texture Texture to render.
+ * @param callback Function to call at the start of each scanline.
+ */
 void mode7_renderer_render(mode7_renderer_t* renderer, texture_t* texture, mode7_callback_t callback);
 
 #endif
