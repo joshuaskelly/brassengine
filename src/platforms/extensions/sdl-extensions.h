044ce4646d0290c1556b1b3490e697d932162afd
Joshua Skelton <joshua.skelton@gmail.com>
2025-08-21 11:53:40 -0700

    Comment pass

--- src/platforms/extensions/sdl-extensions.h
+++ src/platforms/extensions/sdl-extensions.h
@@ -3,6 +3,12 @@
 
 #include <SDL.h>
 
+/**
+ * Gets the scale of a window's pixel.
+ *
+ * @param window The window to query
+ * @return Scale as a ratio of logical to pixel units.
+ */
 float SDL_GetWindowPixelScale(SDL_Window* window) {
     int sw, pw;
     SDL_GetWindowSize(window, &sw, NULL);
@@ -11,10 +17,17 @@ float SDL_GetWindowPixelScale(SDL_Window* window) {
     return (float)sw / pw;
 }
 
-void SDL_SetWindowSizeInPixels(SDL_Window* window, int w, int h) {
+/**
+ * Sets the size of a window's client area in pixels.
+ *
+ * @param window The window to change
+ * @param width Width of window in pixels
+ * @param height Height of window in pixels
+ */
+void SDL_SetWindowSizeInPixels(SDL_Window* window, int width, int height) {
     float pixel_scale = SDL_GetWindowPixelScale(window);
 
-    SDL_SetWindowSize(window, w * pixel_scale, h * pixel_scale);
+    SDL_SetWindowSize(window, width * pixel_scale, height * pixel_scale);
 }
 
 #endif
