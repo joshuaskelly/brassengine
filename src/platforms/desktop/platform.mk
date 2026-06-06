c331a13228c4eef01a16aa15a53f2aff0cdd939f
Joshua Skelton <joshua.skelton@gmail.com>
2026-06-06 15:54:44 -0700

    Fix platform.o target for Mac

--- src/platforms/desktop/platform.mk
+++ src/platforms/desktop/platform.mk
@@ -2,6 +2,9 @@
 
 LDLIBS+=`sdl2-config --libs` -lSDL2_mixer
 
+PLATFORM_OBJ_DIR=$(OBJ_DIR)/platforms/$(PLATFORM)
+PLATFORM_SRC_DIR=$(SRC_DIR)/platforms/$(PLATFORM)
+
 # Platform specific platform.o target
-$(OBJ_DIR)/platform.o: $(SRC_DIR)/platform.c | $(OBJ_DIR)
+$(PLATFORM_OBJ_DIR)/platform.o: $(PLATFORM_SRC_DIR)/platform.c | $(OBJ_DIR)
 	$(CC) $(CFLAGS) $(INC) `sdl2-config --cflags` -c $< -o $@
